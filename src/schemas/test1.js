cbe7256124e4cc835b065ed5d93ee55ff20e2dea
Pieter Wigboldus <w3n@w3news.org>
2021-05-11 20:20:58 +0200

    More tests

--- src/schemas/test1.js
+++ src/schemas/test1.js
@@ -1,10 +1,12 @@
 import Test2 from './test2';
 import Test3Schema from './test3';
+import Test5Schema from './test5';
 
 export default {
     name: String,
     '?test': Test2,
     'test3?': Test3Schema,
     'test4?': 'string|number',
+    '?test5': Test5Schema,
     'obj?': Object,
 };
